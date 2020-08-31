14469bb0694225f4561def9111c945a0f6417ad3
B50gamer <B50@users.noreply.github.com>
2020-08-31 16:15:25 -0700

    Redid Status

--- calebbot.js
+++ calebbot.js
@@ -39,5 +39,18 @@ client.on('message', message =>{
          } 
 });
 
+
+
+// Keep this at the bottom
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+        status: "online",  //You can show online, idle....
+        game: {
+            name: "!youtube, !twitch, !twitter",  //The message shown
+            type: "STREAMING" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
+ });
  
 client.login('NzUwMDYyODkyNjMxNzE5OTU4.X01EgQ.mpKcE--nSipjs7NnRF0C8wDAeJg');
