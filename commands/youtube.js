aa7f59701ac8914d4fac5567b35b0ee205a247da
B50gamer <B50@users.noreply.github.com>
2020-08-31 15:57:48 -0700

    First upload

---
+++ commands/youtube.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'youtube',
+    aliases: 'yt',
+    description: "this is caleb's youtube",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/channel/UCfnISZl8s77Bp4QwHU8UwKw');
+    }
+} 
+module.exports = {
+    name: 'yt',
+    description: "this is caleb's youtube",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/channel/UCfnISZl8s77Bp4QwHU8UwKw');
+    }
+} 
