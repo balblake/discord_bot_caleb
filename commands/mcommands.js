0da3e17d045dcc2b25d6ccb9ebb1fae9a54d9381
B50gamer <B50@users.noreply.github.com>
2020-09-01 09:50:45 -0700

    Update mcommands.js

--- commands/mcommands.js
+++ commands/mcommands.js
@@ -2,6 +2,15 @@ module.exports = {
     name: 'mcommands',
     description: "All the mod commands",
     execute(message, args){
+
+        if(message.member.roles.cache.has('749510080159809586')){
         message.channel.send('!kick (name), !ban (name), !clear (1-99)');
+
+
+
+    } else {
+        message.channel.send('Nice try command is only for mods! ');
+    }
+
     }
 } 
