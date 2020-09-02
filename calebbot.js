39fb28307532ac30fec29d993ddeef8dc9341bfd
B50 <B50@users.noreply.github.com>
2020-09-02 13:46:24 -0500

    LMAO

--- calebbot.js
+++ calebbot.js
@@ -20,9 +20,10 @@ client.on('guildMemberAdd', member => {
     member.roles.add(role);
     });
     client.on('guildMemberAdd', member => {
-    member.guild.channels.cache.get('749511642894565446').send(`Welcome, **${member}** to caleb's discord server!\nMake sure to check out my socials!\n**Twitch                                                       Twitter**\nhttps://twitch.tv/calebftg                https://twitter.com/FTGCaleb`);
-    }); 
- 
+    const targetChannelId = '750147958002942033'
+    member.guild.channels.cache.get('749511642894565446').send(`Welcome, **${member}** to caleb's discord server!\nMake sure to check out his socials by going to ${member.guild.channels.cache.get(targetChannelId).toString()} and typing !socials\n**Twitch                                                       Twitter**\nhttps://twitch.tv/calebftg                https://twitter.com/FTGCaleb`);
+    });
+
 client.once('ready', () => {
     console.log('Caleb Bot is online!');
 });
